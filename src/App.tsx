24fdc28b03efe4a2c668dbed6a3b3669ea4e0e23
Clay McGinnis <mcginnis@claymav.com>
2019-01-30 16:46:23 -0600

    Add react-router and empty tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { Layout } from 'antd';
 import * as React from 'react';
-import { Description, FooterNav, MemberSpot, Nav, SIGs, Sponsors } from './components/index'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Events, Home, Membership, Sigs, Sponsors } from './screens'
+
 
 import './App.css'
-
-const { Content } = Layout
 
 class App extends React.Component {
   public render() {
     return (
-      <Layout className="layout">
-        <Nav/>
-        <Content
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '0 50px'
-          }}
-        >
-          <Description/>
-          <SIGs/>
-          <Sponsors/>
-          <MemberSpot/>
-        </Content>
-        <FooterNav/>
-      </Layout>
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={Home}/>
+          <Route path="/tools/membership" component={Membership}/>
+          <Route path="/tools/events" component={Events}/>
+          <Route path="/tools/sigs" component={Sigs}/>
+          <Route path="/tools/sponsors" component={Sponsors}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
+  
+// <Route path="/login" component={Login}/>
 
 export default App;
