8039ce9a063ca1ee1767d0f8799cfe5748ecb009
Clay McGinnis <mcginnis@claymav.com>
2019-04-07 17:15:35 -0500

    Added editting of events

--- src/screens/Tools/Events/index.tsx
+++ src/screens/Tools/Events/index.tsx
@@ -1,6 +1,15 @@
 import moment from "moment";
+import Modal from "react-modal";
 import * as React from "react"
 import styled from "styled-components"
+
+Modal.setAppElement('#root')
+
+interface ISig {
+  dateFounded: Date;
+  description: string;
+  name: string;
+}
 
 interface IEvent {
   dateCreated: Date;
@@ -12,17 +21,25 @@ interface IEvent {
   flierLink: string;
   id: number;
   location: string;
+  hostSigs: ISig;
 }
 
 interface IEventState {
   error: any;
   events: IEvent[];
   isLoaded: boolean;
+  modalIsOpen: boolean;
+  modalId: number | undefined;
+  editData: any;
 }
 
 interface IEventProps {
   event: IEvent;
   key: number;
+  editEvent: any;
+}
+
+interface IEventState {
 }
 
 const PageWrapper = styled.div`
@@ -74,44 +91,75 @@ function formatDate(date: Date) {
   return moment(date).format("MMMM Do h:mmA");
 }
 
-const Event = (props: IEventProps) => {
-  const { event } = props;
-  return (
-    <EventWrapper>
-      <EventContent>
-        <EventFlier src={event.eventLink} alt={"Flier"} />
-        <EventDetails>
-          <EventHighLevel>
-            <EventTitle>{event.eventTitle}</EventTitle>
-            <EventDate>
-              {formatDate(event.dateHosted)}{event.dateHosted !== event.dateExpire && (" - " + formatDate(event.dateExpire))}
-            </EventDate>
-          </EventHighLevel>
-          <EventMidLevel>
-            {event.location}{event.eventLink && (<span> - <a href={event.eventLink}>Link</a></span>)}
-          </EventMidLevel>
-          <EventDescription>{event.description}</EventDescription>
-          <EventLowLevel>{formatDate(event.dateCreated)}</EventLowLevel>
-        </EventDetails>
-      </EventContent>
-      <div>
-        Hello
-      </div>
-    </EventWrapper>
-  );
+class Event extends React.Component<IEventProps, IEventState> {
+  public constructor(props: IEventProps) {
+    super(props)
+  }
+
+  handleEdit = () => {
+    this.props.editEvent(this.props.event.id, this.props.event);
+  }
+
+  render() {
+    const { event } = this.props;
+    return (
+      <EventWrapper>
+        <EventContent>
+          <EventFlier src={event.eventLink} alt={"Flier"} />
+          <EventDetails>
+            <EventHighLevel>
+              <EventTitle>ACM {event.hostSigs.name}: {event.eventTitle}</EventTitle>
+              <EventDate>
+                {formatDate(event.dateHosted)}{event.dateHosted !== event.dateExpire && (" - " + formatDate(event.dateExpire))}
+              </EventDate>
+            </EventHighLevel>
+            <EventMidLevel>
+              {event.location}{event.eventLink && (<span> - <a href={event.eventLink}>Link</a></span>)}
+            </EventMidLevel>
+            <EventDescription>{event.description}</EventDescription>
+            <EventLowLevel>{formatDate(event.dateCreated)}</EventLowLevel>
+          </EventDetails>
+        </EventContent>
+        <div>
+          Hello
+          <button onClick={this.handleEdit}>Edit</button>
+        </div>
+      </EventWrapper>
+    );
+  }
 }
 
 class Events extends React.Component<{}, IEventState> {
   public constructor(props: {}) {
     super(props)
+
     this.state = {
       error: null,
       events: [],
-      isLoaded: false
+      isLoaded: false,
+      modalIsOpen: false,
+      modalId: undefined,
+      editData: {}
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.changeValue = this.changeValue.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  closeModal() {
+    this.setState({
+      modalId: undefined,
+      modalIsOpen: false,
+      editData: {}
+    });
   }
 
   public componentDidMount() {
+    this.refresh();
+  }
+
+  private refresh = () => {
     fetch("http://localhost/api/v1/events/")
       .then(res => res.json())
       .then(
@@ -130,9 +178,78 @@ class Events extends React.Component<{}, IEventState> {
       )
   }
 
+  private editEvent = (id: number, data: any) => {
+    data["hostSigs"] = data["hostSigs"].name
+    this.setState({
+      modalId: id,
+      modalIsOpen: true,
+      editData: data
+    });
+  }
+
+  private patchEvent = (event: any) => {
+    event.preventDefault()
+    console.log(this.state.editData)
+    fetch("http://localhost/api/v1/events/" + this.state.modalId,
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "PATCH",
+        body: JSON.stringify(this.state.editData)
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.closeModal()
+          this.refresh()
+        },
+        (error) => {
+          // TODO
+        }
+      )
+  }
+
+  changeValue = (field: string, event: any) => {
+    let { editData } = this.state;
+    editData[field] = event.target.value
+    this.setState({editData: editData});
+  }
+
+  handleChange = (field: string) => {
+    return (event: any) => this.changeValue(field, event)
+  }
+
+
   public render() {
-    const { events, isLoaded, error } = this.state;
+    const { events, isLoaded, error, modalIsOpen, modalId, editData } = this.state;
     let content;
+    const modal = (
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={this.closeModal}
+        contentLabel={"Edit" + modalId}
+      >
+
+        <button onClick={this.closeModal}>close</button>
+        <form onSubmit={this.patchEvent}>
+          <input onChange={this.handleChange("id")} value={editData.id}/>
+          <input onChange={this.handleChange("eventTitle")} value={editData.eventTitle}/>
+          <input onChange={this.handleChange("description")} value={editData.description}/>
+          <input onChange={this.handleChange("location")} value={editData.location}/>
+          <input onChange={this.handleChange("flierLink")} value={editData.flierLink}/>
+          <input onChange={this.handleChange("eventLink")} value={editData.eventLink}/>
+          <input type="submit" value="Submit" />
+        </form>
+      </Modal>
+    )
+    /*
+          Add these to the form
+          "dateCreated": "2019-04-07T18:43:13.087Z",
+          "dateHosted": "2019-04-07T13:43:13.033Z",
+          "dateExpire": "2019-04-07T13:43:13.033Z",
+    */
 
     if (!isLoaded) {
       content = (
@@ -145,7 +262,7 @@ class Events extends React.Component<{}, IEventState> {
     } else {
       content = events.map((event: IEvent, index: number) => {
           return (
-            <Event event={event} key={index} />
+            <Event event={event} editEvent={this.editEvent} key={index} />
           )
       })
     }
@@ -153,6 +270,7 @@ class Events extends React.Component<{}, IEventState> {
     return (
       <PageWrapper>
         <h1>Events</h1>
+        {modal}
         <EventsList>
           {content}
         </EventsList>
