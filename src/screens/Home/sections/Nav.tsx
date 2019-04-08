d9b72de9b3ce079374890c2e641f427d86d10664
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-04-08 13:47:09 -0500

    Add google login

--- src/screens/Home/sections/Nav.tsx
+++ src/screens/Home/sections/Nav.tsx
@@ -1,23 +1,62 @@
-import { Button, Layout, Menu } from 'antd';
-import * as React from 'react';
+import { Button, Layout, Menu } from "antd";
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline
+} from "react-google-login";
+import * as React from "react";
 
-const { Header } = Layout
+const { Header } = Layout;
 
 class Nav extends React.Component {
+  private async submitGoogleToken(
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) {
+    const GOOGLE_LOGIN: GoogleLoginResponse = response as GoogleLoginResponse;
+    const ID_TOKEN: string = GOOGLE_LOGIN.getAuthResponse().id_token;
+    console.log(ID_TOKEN);
+  }
+
   public render() {
+    // See .venv in project root
+    const GOOGLE_ID: string = process.env.REACT_APP_GOOGLE_CLIENT_ID || "";
+    const error = (response: any) => console.error(response);
+
     return (
       <Header>
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['1']}
-          style={{ lineHeight: '64px' }}
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "64px" }}
         >
           <Menu.Item key="1">Who Are We?</Menu.Item>
-          <Menu.Item key="2" disabled={true}>Events</Menu.Item>
-          <Menu.Item key="3" disabled={true}>Sponsors</Menu.Item>
-          <Menu.Item key="4" disabled={true}>Officers</Menu.Item>
-          <Button style={{ float: 'right' }}>Sign In</Button>
+          <Menu.Item key="2" disabled={true}>
+            Events
+          </Menu.Item>
+          <Menu.Item key="3" disabled={true}>
+            Sponsors
+          </Menu.Item>
+          <Menu.Item key="4" disabled={true}>
+            Officers
+          </Menu.Item>
+          <GoogleLogin
+            clientId={GOOGLE_ID}
+            prompt="consent"
+            hostedDomain="mst.edu"
+            render={(renderProps?: { onClick: () => void }) => (
+              <Button
+                onClick={renderProps ? renderProps.onClick : undefined}
+                disabled={renderProps ? false : true}
+                style={{ float: "right" }}
+              >
+                Sign In
+              </Button>
+            )}
+            buttonText="Login"
+            onSuccess={this.submitGoogleToken}
+            onFailure={error}
+          />
         </Menu>
       </Header>
     );
--- src/screens/Home/sections/Sponsors.tsx
+++ src/screens/Home/sections/Sponsors.tsx
@@ -1,31 +1,32 @@
-import { Button } from 'antd'
-import * as React from 'react'
-import styled from 'styled-components'
-import Sponsor from './Sponsor'
+import * as React from "react";
+
+import { Button } from "antd";
+import styled from "styled-components";
+import Sponsor from "./Sponsor";
 
 const SponsorsWrapper = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-background: grey;
-margin: auto;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: grey;
+  margin: auto;
+`;
 const CardHolder = styled.div`
-display: flex;
-flex-wrap: wrap;
-`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const SponsorList = [
   {
-    "name": "Tradebot",
-    "tier": "Sponsor"
+    name: "Tradebot",
+    tier: "Sponsor"
   },
   {
-    "name": "Garmin",
-    "tier": "Sponsor"
+    name: "Garmin",
+    tier: "Sponsor"
   }
-]
+];
 
 class Sponsors extends React.Component {
   public render() {
@@ -33,7 +34,7 @@ class Sponsors extends React.Component {
       <SponsorsWrapper>
         <CardHolder>
           {SponsorList.map((sponsor, index) => (
-            <Sponsor sponsor={sponsor} key={index}/>
+            <Sponsor sponsor={sponsor} key={index} />
           ))}
         </CardHolder>
         <Button>Learn More</Button>
